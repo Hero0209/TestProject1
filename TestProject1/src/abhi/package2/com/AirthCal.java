f568e19142792b2b3714a206a3d55401a020cfcb
Abhi <abhis.teacher@gmail.com>
2022-06-15 08:36:48 +0530

    First Commit

---
+++ TestProject1/src/abhi/package2/com/AirthCal.java
@@ -0,0 +1,14 @@
+package abhi.package2.com;
+
+public class AirthCal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int first_num=10;
+		int second_num=30;
+		int sum=first_num + second_num;
+		System.out.println("Sum ="+sum);
+	}
+
+}
